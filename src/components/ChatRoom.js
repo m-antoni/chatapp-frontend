7ac4cf3b3927c8ea5323d6b8a85f20d5906685c3
Michael Antoni <michaelantoni.cs@gmail.com>
2021-08-30 19:31:24 +0800

    initial commit frontend chat app

---
+++ src/components/ChatRoom.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function ChatRoom() {
+    return (
+        <div>
+            <h1>Choose ChatRoom</h1>
+            <select>
+                <option value="">JavaScript</option>
+                <option value="">React</option>
+                <option value="">TypeScript</option>
+                <option value="">Node/Express</option>
+                <option value="">Node/Nest</option>
+                <option value="">Angular</option>
+            </select>
+        </div>
+    )
+}
+
+export default ChatRoom
