a86058b98ebad1d79aaf41daf8abc88844e45a8f
Michael Antoni <michaelantoni.cs@gmail.com>
2021-09-09 22:21:45 +0800

    update chatroom ui

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -7,21 +7,35 @@ import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
 import { CardContent, List, ListItem, Typography, ListItemText, Divider, Avatar, ListItemAvatar, Box} from '@material-ui/core';
 import Message from './Message';
-
+import SendIcon from '@material-ui/icons/Send';
 
 const useStyles = makeStyles({
-    cardStyle: {
-        marginTop: '70px'
+    cardStyle: { 
+        position: 'absolute',
+        bottom: '0',
+        marginTop: '90px',
+        maxHeight: '90vh',
+        marginBottom: '20px',
+        marginLeft: '-5px',
+        marginRight: '10px',
+        overflowY: 'scroll',
+        backgroundImage: '-webkit-gradient(linear, right top, left top, from(#9f78ff), to(#32cafe))',
+        backgroundImage: '-webkit-linear-gradient(left, #9f78ff, #32cafe)',
+        backgroundImage: '-moz-linear-gradient(left, #9f78ff, #32cafe)',
+        backgroundImage: '-o-linear-gradient(left, #9f78ff, #32cafe)',
+        backgroundImage: 'linear-gradient(to right, #9f78ff, #32cafe)',
+        backgroundRepeat: 'repeat-x',
     },
     headerText: {
         marginBottom: '20px'
     },
     ButtonStyle: {
         marginLeft: '10px',
-        height: '55px'
+        height: '55px',
+        // width: '80px'
     },
-    ListRight: {
-        marginLeft: 'auto'
+    box: {
+        marginTop: '20px'
     }
 })
 
@@ -42,20 +56,16 @@ function ChatRoom({ username, roomname, socket }) {
 
     return (
         <Container maxWidth="sm">
-            <Grid>
-                <Grid item xs={12}>
-                    <Card className={classes.cardStyle}>
-                        <CardContent>
-                            <Typography className={classes.headerText}>Room: {roomname}</Typography>
-                            <Message messages={messages} username={username} socket={socket}/>
-                            <Box display="flex" justifyContent="start" alignItems="center"> 
-                                <TextField onChange={e => setText(e.target.value)} name="text" value={text} variant="outlined" label="Message" fullWidth size="medium"></TextField>
-                                <Button type="submit" className={classes.ButtonStyle} variant="contained" color="primary" size="medium"> Send</Button>
-                            </Box>
-                        </CardContent>
-                    </Card>
-                </Grid>
-            </Grid>
+            <Card className={classes.cardStyle}>
+                <CardContent>
+                    <Typography className={classes.headerText}>Room: {roomname}</Typography>
+                    <Message messages={messages} username={username} socket={socket}/>
+                    <Box className={classes.box} display="flex" justifyContent="start" alignItems="center"> 
+                        <TextField onChange={e => setText(e.target.value)} name="text" value={text} variant="outlined" label="Message" fullWidth size="medium"></TextField>
+                        <Button type="submit" className={classes.ButtonStyle} variant="contained" color="primary" size="medium"> <SendIcon/> </Button>
+                    </Box>
+                </CardContent>
+            </Card>
         </Container>
     )
 }
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { makeStyles, ThemeProvider   } from '@material-ui/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Typography from '@material-ui/core/Typography'
 import { deepOrange, deepPurple } from '@material-ui/core/colors';
+
 
 const useStyles = makeStyles((theme) => ({
     orange: {
@@ -12,25 +13,49 @@ const useStyles = makeStyles((theme) => ({
     purple: {
       color: 'white',
       backgroundColor: deepPurple[500],
+      marginRight: '6px'   
     },
     messageRight: {
-        width: '50%',
+        width: '75%',
         display: 'flex',
         justifyContent: 'flex-start',
         marginLeft: 'auto',
         fontSize: '0.9rem',
         backgroundColor: '#f8e896',
-        padding: '8px',
-        borderRadius: '10px',
+        padding: '10px',
+        borderRadius: '20px 20px 0px 20px',
         margin: '10px 0px'
-    }
+    },
+    messageLeft: {
+        width: '75%',
+        display: 'flex',
+        justifyContent: 'flex-start',
+        marginRight: 'auto',
+        fontSize: '0.9rem',
+        backgroundColor: '#A8DDFD',
+        padding: '10px',
+        borderRadius: '20px 20px 20px 0px',
+        margin: '10px 0px'
+    },
 }));
 
-// #A8DDFD
+
+
+
 function Message({ messages, username }){
 
     console.log(messages, username)
+    
     const classes = useStyles();
+
+    const messagesEndRef = useRef(null);
+
+    const scrollToBottom = () => {
+        messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+    };
+
+    useEffect(scrollToBottom, [messages]);
+
     
     return ( 
        <>
@@ -42,17 +67,66 @@ function Message({ messages, username }){
                         )
                     }else{
                         return (
+                            <>
+                            <div className={classes.messageLeft}>
+                                <Avatar className={classes.purple}>M</Avatar>
+                                <div>
+                                    <div>Lorem Ipsum akjsdkalsj kasjdklasjdkl ajlkdja asdasd asdasd adasklsjd</div>
+                                </div>
+                            </div>
+                 
                             <div className={classes.messageRight}>
                                 <div>
                                     <div>Lorem Ipsum akjsdkalsj kasjdklasjdkl ajlkdja asdasd asdasd adasklsjd</div>
                                 </div>
                                 <Avatar className={classes.orange}>M</Avatar>
                             </div>
-                            
+                            <div className={classes.messageLeft}>
+                                <Avatar className={classes.purple}>M</Avatar>
+                                <div>
+                                    <div>Lorem Ipsum akjsdkalsj kasjdklasjdkl ajlkdja asdasd asdasd adasklsjd</div>
+                                </div>
+                            </div>
+                 
+                            <div className={classes.messageRight}>
+                                <div>
+                                    <div>Lorem Ipsum akjsdkalsj kasjdklasjdkl ajlkdja asdasd asdasd adasklsjd</div>
+                                </div>
+                                <Avatar className={classes.orange}>M</Avatar>
+                            </div>
+         
+                            <div className={classes.messageLeft}>
+                                <Avatar className={classes.purple}>M</Avatar>
+                                <div>
+                                    <div>Lorem Ipsum akjsdkalsj kasjdklasjdkl ajlkdja asdasd asdasd adasklsjd</div>
+                                </div>
+                            </div>
+                 
+                            <div className={classes.messageRight}>
+                                <div>
+                                    <div>Lorem Ipsum akjsdkalsj kasjdklasjdkl ajlkdja asdasd asdasd adasklsjd</div>
+                                </div>
+                                <Avatar className={classes.orange}>M</Avatar>
+                            </div>
+                            <div className={classes.messageLeft}>
+                                <Avatar className={classes.purple}>M</Avatar>
+                                <div>
+                                    <div>Lorem Ipsum akjsdkalsj kasjdklasjdkl ajlkdja asdasd asdasd adasklsjd</div>
+                                </div>
+                            </div>
+                 
+                            <div className={classes.messageRight}>
+                                <div>
+                                    <div>Lorem Ipsum akjsdkalsj kasjdklasjdkl ajlkdja asdasd asdasd adasklsjd</div>
+                                </div>
+                                <Avatar className={classes.orange}>M</Avatar>
+                            </div>
+                            </>
                         )
                     }
                 })
             }
+             <div ref={messagesEndRef} />
        </>
     );
 }
